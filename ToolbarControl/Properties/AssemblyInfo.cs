bce8bfe71338580e91b61265c3f7236b8a420896
Lisias T <me@lisias.net>
2018-08-08 13:29:59 -0300

    Normalizing Versioning

--- ToolbarControl/Properties/AssemblyInfo.cs
+++ ToolbarControl/Properties/AssemblyInfo.cs
@@ -33,6 +33,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion(ToolbarControl_NS.PluginVersion.Number)]
+[assembly: AssemblyVersion(ToolbarControl_NS.PluginVersion.Number)]
 
 [assembly: KSPAssembly("ToolbarController", 1, 0)]
