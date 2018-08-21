7551e2c44fe68aafc08cba4a8e68fed4b6c715d6
Jonathan Bayer <linuxgurugamer@gmail.com>
2018-08-21 12:07:21 -0400

    Fixed timing issue on toolbar registration  Added log warning in case RegisterToolbar is called too late

--- ToolbarControl/ConfigInfo.cs
+++ ToolbarControl/ConfigInfo.cs
@@ -19,7 +19,6 @@ namespace ToolbarControl_NS
 
         void Start()
         {
-            Log.Info("ConfigInfo.Start");
             Instance = this;
             LoadData();
             DontDestroyOnLoad(this);
@@ -27,7 +26,6 @@ namespace ToolbarControl_NS
 
         public void SaveData()
         {
-            Log.Info("ToolbarControl.SaveData");
             ConfigNode settingsFile = new ConfigNode();
             ConfigNode settings = new ConfigNode();
 
@@ -40,8 +38,7 @@ namespace ToolbarControl_NS
 
         public void LoadData()
         {
-            Log.Info("ToolbarControl.LoadData");
-            ConfigNode settingsFile = ConfigNode.Load(DEBUGCFG);
+            ConfigNode settingsFile = ConfigNode.Load(Settings.PLUGINDATA);
             ConfigNode node = null;
             if (settingsFile != null)
             {
