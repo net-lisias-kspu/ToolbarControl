fd4666b5334a5d684819e7dee6416aff715e1838
Lisias T <me@lisias.net>
2019-07-21 20:22:11 -0300

    Legal Mambo Jambo

--- Source/ToolbarControl/Properties/AssemblyInfo.cs
+++ Source/ToolbarControl/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ToolbarControl")]
-[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyCopyright("Copyright © 2019 LinuxGuruGamer, LisiasT")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
